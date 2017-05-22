d16cbd9abe559c5061f5778eabcdbf5487051600
james mc <jleo2525@live.com>
2017-05-22 18:25:06 +0000

    passing options and printing info for ls

--- ls_adv/ls.c
+++ ls_adv/ls.c
@@ -1,6 +1,7 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 static const struct option long_options[] = 
 {
@@ -18,11 +19,23 @@ static const struct option long_options[] =
 	}
 };
 
+typedef struct 
+{
+	int l;
+	int a;
+	int d;
+} ls_opts;
+
+void _print_ls(ls_opts options);
+
 int main(int argc, char *argv[])
 {
 	char *optstr = "lad";
 	int long_opts_i = 0;
-
+	ls_opts ls_options = {
+		.l = -1, .a = -1, .d = -1	
+	};
+	
 	while(1)
 	{
 		int c = getopt_long(argc, argv, optstr, long_options, &long_opts_i);
@@ -31,16 +44,13 @@ int main(int argc, char *argv[])
 		switch(c)
 		{
 			case 'l':
-				putchar(c);
-				printf("%s\n", long_options[long_opts_i].name);
+				ls_options.l = 1;
 				break;
 			case 'a':
-				putchar(c);
-				printf("%s\n", long_options[long_opts_i].name);
+				ls_options.a = 1;
 				break;
 			case 'd':
-				putchar(c);
-				printf("%s\n", long_options[long_opts_i].name);
+				ls_options.d = 1;
 				break;
 			default:
 				putchar(c);
@@ -49,6 +59,26 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	_print_ls(ls_options);
+
 	return 0;
 }
 
+void _print_ls(ls_opts options)
+{
+	DIR *dir_p;  
+	struct dirent *dirent_p;
+
+	if ((dir_p = opendir(".")) == NULL) 
+	{
+		perror("opendir");
+		exit(-1);
+	}
+
+	while ((dirent_p = readdir(dir_p)) != NULL)
+	{
+		printf("%s\n", dirent_p->d_name);
+	}
+	
+	closedir(dir_p);
+}	
