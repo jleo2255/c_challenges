0c79d218348a28fe5ec72254dcef6b588556c78e
james <jleo2255@live.com>
2017-07-27 09:59:45 -0400

    adding win32 impl of non-recursive find in c

---
+++ mthread_find/win32find/win32find/win32find.cpp
@@ -0,0 +1,100 @@
+// win32find.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void _check_invariant_conditions(int argc, char *argv[])
+{
+	if (argc == 1 || argc > 3)
+	{
+		fprintf(stderr, "usage: %s [starting point...] [expression]\n", argv[0]);
+		exit(-1); // argument length
+	}
+	if (strlen(argv[1]) > MAX_PATH)
+	{
+		fprintf(stderr, "argument length: path cannot exceed %d characters\n", MAX_PATH);
+		exit(-1);
+	}
+	if (argc > 3 && strlen(argv[2]) > FILENAME_MAX)
+	{
+		fprintf(stderr, "argument length: expression cannot exceed %d characters\n", FILENAME_MAX);
+		exit(-1);
+	}
+}
+
+void _search(wchar_t base_path[], wchar_t filename[])
+{
+	LPWIN32_FIND_DATA hFile = (LPWIN32_FIND_DATA) GlobalAlloc(GMEM_ZEROINIT, sizeof(WIN32_FIND_DATA));
+	HANDLE findResult = NULL;
+
+	wchar_t combined_filename[MAX_PATH] = TEXT("");
+
+	StringCchCat(combined_filename, MAX_PATH - FILENAME_MAX - 1, base_path);
+	StringCchCat(combined_filename, 1, TEXT("/"));
+	StringCchCat(combined_filename, FILENAME_MAX, filename);
+	
+	findResult = FindFirstFile((LPCWSTR)combined_filename, hFile);
+	
+	if (findResult == INVALID_HANDLE_VALUE)
+	{
+		DWORD dw_error_code = 0;
+
+		dw_error_code = GetLastError();
+
+		if (dw_error_code == ERROR_FILE_NOT_FOUND)
+		{
+			_tprintf(TEXT("No results to display.\n"));
+		}
+		else
+		{
+			_tprintf(TEXT("FindFirstFile failed %d\n"), dw_error_code);
+		}
+	}
+
+	do 
+	{
+		_tprintf(TEXT("%s\n"), hFile->cFileName);
+	} while (FindNextFile(findResult, hFile));
+
+	FindClose(findResult);
+}
+
+int main(int argc, char *argv[])
+{
+	_check_invariant_conditions(argc, argv);
+
+	wchar_t base_path[MAX_PATH] = L"";
+	wchar_t file_name[FILENAME_MAX] = L"";
+
+	if (argc == 2)
+	{
+		wchar_t fn[FILENAME_MAX];
+		mbstowcs_s(NULL, fn, argv[1], FILENAME_MAX);
+
+		StringCchCat(base_path, 4, L".\\");
+		StringCchCat(file_name, FILENAME_MAX * sizeof(wchar_t), fn);
+	}
+	else if (argc == 3)
+	{
+		wchar_t fn[FILENAME_MAX * sizeof(wchar_t)];
+		mbstowcs_s(NULL, fn, argv[2], FILENAME_MAX);
+
+		wchar_t bp[MAX_PATH * sizeof(wchar_t)];
+		mbstowcs_s(NULL, bp, argv[1], FILENAME_MAX);
+
+		StringCchCat(base_path, MAX_PATH * sizeof(wchar_t), bp);
+		StringCchCat(file_name, FILENAME_MAX * sizeof(wchar_t), fn);
+	}
+
+	_search(base_path, file_name);
+
+	return 0;
+}
