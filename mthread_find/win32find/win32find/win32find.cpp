d30602dd34eeab9d8226e5469c6b6994b7c688f9
james <jleo2525@live.com>
2017-07-30 10:38:27 -0400

    making find recursive

--- mthread_find/win32find/win32find/win32find.cpp
+++ mthread_find/win32find/win32find/win32find.cpp
@@ -32,28 +32,28 @@ void _check_invariant_conditions(int argc, char *argv[])
 
 void _search(wchar_t base_path[], wchar_t filename[])
 {
-	LPWIN32_FIND_DATA hFile = (LPWIN32_FIND_DATA) GlobalAlloc(GMEM_ZEROINIT, sizeof(WIN32_FIND_DATA));
-	HANDLE findResult = NULL;
-
-	wchar_t combined_filename[MAX_PATH] = TEXT("");
-
-	StringCchCat(combined_filename, MAX_PATH - FILENAME_MAX - 1, base_path);
-	StringCchCat(combined_filename, 1, TEXT("/"));
-	StringCchCat(combined_filename, FILENAME_MAX, filename);
+	LPWIN32_FIND_DATA hFile = (LPWIN32_FIND_DATA)GlobalAlloc(GMEM_ZEROINIT, sizeof(WIN32_FIND_DATA));
+	LPWIN32_FIND_DATA hDirectory = (LPWIN32_FIND_DATA) GlobalAlloc(GMEM_ZEROINIT, sizeof(WIN32_FIND_DATA));
 	
-	findResult = FindFirstFile((LPCWSTR)combined_filename, hFile);
-	
-	if (findResult == INVALID_HANDLE_VALUE)
+	HANDLE findFileResult = NULL;
+	HANDLE findDirectoryResult = NULL;
+
+	wchar_t wildcard[MAX_PATH] = L"";
+
+	size_t bp_sz = wcsnlen_s(base_path, MAX_PATH);
+
+	StringCchCat(wildcard, bp_sz + 1, base_path);
+	StringCchCat(wildcard, bp_sz + 2, L"*");
+
+	findDirectoryResult = FindFirstFile((LPCWSTR)wildcard, hDirectory);
+
+	if (findDirectoryResult == INVALID_HANDLE_VALUE)
 	{
 		DWORD dw_error_code = 0;
 
 		dw_error_code = GetLastError();
 
-		if (dw_error_code == ERROR_FILE_NOT_FOUND)
-		{
-			_tprintf(TEXT("No results to display.\n"));
-		}
-		else
+		if (dw_error_code != ERROR_FILE_NOT_FOUND)
 		{
 			_tprintf(TEXT("FindFirstFile failed %d\n"), dw_error_code);
 		}
@@ -61,10 +61,57 @@ void _search(wchar_t base_path[], wchar_t filename[])
 
 	do 
 	{
-		_tprintf(TEXT("%s\n"), hFile->cFileName);
-	} while (FindNextFile(findResult, hFile));
+		if (hDirectory->dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY
+			&& wcscmp(hDirectory->cFileName, L".") != 0
+			&& wcscmp(hDirectory->cFileName, L"..") != 0)
+		{
+			wchar_t new_bp[MAX_PATH] = L"";
 
-	FindClose(findResult);
+			size_t fn_sz = wcsnlen_s(hDirectory->cFileName, FILENAME_MAX);
+
+			StringCchCat(new_bp, bp_sz + 1, base_path);
+			StringCchCat(new_bp, bp_sz + fn_sz + 1, hDirectory->cFileName);
+			StringCchCat(new_bp, bp_sz + fn_sz + 2, L"\\");
+			
+			_search(new_bp, filename);
+		}
+		
+	} while (FindNextFile(findDirectoryResult, hDirectory));
+
+	wchar_t combined_filename[MAX_PATH] = TEXT("");
+
+	StringCchCat(combined_filename, bp_sz + 1, base_path);
+	StringCchCat(combined_filename, bp_sz + 2, TEXT("\\"));
+	StringCchCat(combined_filename, bp_sz + wcsnlen_s(filename, FILENAME_MAX) + 2, filename);
+	
+	findFileResult = FindFirstFile((LPCWSTR)combined_filename, hFile);
+	
+	if (findFileResult == INVALID_HANDLE_VALUE)
+	{
+		DWORD dw_error_code = 0;
+
+		dw_error_code = GetLastError();
+
+		if (dw_error_code != ERROR_FILE_NOT_FOUND)
+		{
+			_tprintf(TEXT("FindFirstFile failed %d\n"), dw_error_code);
+			return;
+		}
+
+		return;
+	}
+
+	do 
+	{
+		wchar_t fpn[MAX_PATH] = L"";
+		StringCchCat(fpn, bp_sz + 1, base_path);
+		StringCchCat(fpn, bp_sz + 2, L"\\");
+		StringCchCat(fpn, bp_sz + wcsnlen_s(hFile->cFileName, FILENAME_MAX) + 2, hFile->cFileName);
+
+		_tprintf(TEXT("%s\n"), fpn);
+	} while (FindNextFile(findFileResult, hFile));
+
+	FindClose(findFileResult);
 }
 
 int main(int argc, char *argv[])
@@ -79,7 +126,7 @@ int main(int argc, char *argv[])
 		wchar_t fn[FILENAME_MAX];
 		mbstowcs_s(NULL, fn, argv[1], FILENAME_MAX);
 
-		StringCchCat(base_path, 4, L".\\");
+		StringCchCat(base_path, sizeof(wchar_t) * 3, L".\\");
 		StringCchCat(file_name, FILENAME_MAX * sizeof(wchar_t), fn);
 	}
 	else if (argc == 3)
